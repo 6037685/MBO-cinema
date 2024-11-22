3ca95ee7c793f05f8dd65ed11318641a56b98a67
JulKwZ <julianhoek9@gmail.com>
2024-11-22 14:01:04 +0100

    Homepage Begin

---
+++ bioscoop/html/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+    <html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Kishan">
+        <meta name="keywords" content="">
+        <title>Mbo cinema</title>
+        <link rel="stylesheet" type="text/css" href="Css/styl.css">
+    </head>
+    <body>
+        <section class="headerBox">
+            <header>
+                <nav class="menu">
+                    <h1>MBO<br>Cinema</h1>
+                    <article>
+                        <a>text</a>
+                        <a>text</a>
+                        <a>text</a>
+                        <a>text</a>
+                    </article>
+                    <article></article>
+                    <article></article>
+                    <article>
+                        <img width="45px" src="img/Search.png" alt="Search icoon">
+                        <button>Inloggen</button>
+                    </article>
+                    
+                </nav>
+            </header>
+        </section>
+<main>
+    <section>
+    </section>
+</main>
+<footer>
+
+</footer>
+    </body>
+    </html>
