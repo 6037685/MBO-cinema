c5130f75d62ef7dedf42c0359ef1f415a7aa7a73
JulKwZ <julianhoek9@gmail.com>
2024-12-03 17:38:38 +0100

    Homepage Aangepast
    Straks links werkend maken en de pagina responsive

---
+++ bioscoop/html/Home.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+    <html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Kishan & Julian">
+        <meta name="keywords" content="">
+        <title>Mbo cinema</title>
+        <link rel="stylesheet" type="text/css" href="Css/styl.css">
+        <script defer src="js/index.js"></script>
+    </head>
+    <body>
+        <section id="headerBox">
+            <header id="head">
+                <nav class="menu">
+                    <h1>MBO<br>Cinema</h1>
+                    <article>
+                        <a>text</a>
+                        <a>text</a>
+                        <a>text</a>
+                        <a>text</a>
+                    </article>
+                    <article></article>
+                    <article></article>
+                    <article>
+                        <img width="45px" src="img/Search.png" alt="Search icoon">
+                        <button>Inloggen</button>
+                    </article>
+                </nav>
+            </header>
+        </section>
+<main>
+    <section class="Placeholder">
+        <h1></h1>
+    </section>
+    <section class="movie-container">
+        <h1 id="h1-films">
+            <a href="#">Populaire Films<i>></i></a>
+        </h1>
+            <section class="article-container">
+                <article class="movie"></article>
+                <article class="movie"></article>
+                <article class="movie"></article>
+                <article class="movie"></article>
+                <article class="movie"></article>
+                <article class="movie"></article>
+                <article class="movie"></article>
+            </section>
+        </section>
+        <section class="info-section">
+            <article id="info-article">
+                <img src="img/Circle-placeholder.png">
+                <h1>Beveiligd</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta incidunt voluptate nostrum ullam delectus ut, eum consectetur atque accusantium eius culpa suscipit nobis voluptatum aut distinctio, fugiat magni debitis non.</p>
+            </article>
+            <article id="info-article">
+                <img src="img/Circle-placeholder.png">
+                <h1>Gemakkelijk tickets reserveren</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta incidunt voluptate nostrum ullam delectus ut, eum consectetur atque accusantium eius culpa suscipit nobis voluptatum aut distinctio, fugiat magni debitis non.</p>
+            </article>
+            <article id="info-article">
+                <img src="img/Circle-placeholder.png">
+                <h1>Contact</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta incidunt voluptate nostrum ullam delectus ut, eum consectetur atque accusantium eius culpa suscipit nobis voluptatum aut distinctio, fugiat magni debitis non.</p>
+            </article>
+        </section>
+</main>
+        <footer>
+            <article id="socials">
+                <a href="#"><img src="img/Insta.png" alt="Instagram"></a>
+                <a href="#"><img src="img/Twitter.png" alt="Twitter"></a>
+                <a href="#"><img src="img/LinkedIn.png" alt="LinkedIn"></a>
+            </article>
+            <article>
+                <a href="#">Home</a>
+                <a href="#">Films</a>
+                <a href="#">Beveiliging</a></a>
+                <a href="#">Contact</a>
+            </article>
+            <article></article>
+            <article>
+                <h1>© 2024-MBO | MBOCinemas.nl</h1>
+            </article>
+        </footer>
+    </body>
+</html>
