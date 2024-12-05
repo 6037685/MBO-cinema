be006b8ea3d3f53333bc95a1f639f5d83f3f64fd
kishan <joeko@hotmail.com>
2024-12-05 15:42:30 +0100

    login page gemaakt

--- bioscoop/html/login.php
+++ bioscoop/html/login.php
@@ -1,51 +1,45 @@
 <!DOCTYPE html>
-    <html lang="nl">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="Kishan & Julian">
-        <meta name="keywords" content="">
-        <title>Mbo cinema</title>
-        <link rel="stylesheet" type="text/css" href="Css/styl.css">
-        <script defer src="js/index.js"></script>
-    </head>
-    <body>
-       <?php 
-       
-       include 'header.php';
-       
-       ?>
-<main>
-    <section class="login-container">
-        <article class="login-form">    
-            <h2>Login form</h2>
-            <form>
-                <label for="username">Gebruikersnaam</label>
-                <input type="text" id="username" name="username" placeholder="Gebruikersnaam">
-                
-                <label for="password">Wachtwoord</label>
-                <input type="password" id="password" name="password" placeholder="Wachtwoord">
-                
-                <article class="forgot-password">
-                    <a href="#">Wachtwoord vergeten</a>
-                </article>
-                
-                <button type="submit">Login</button>
-            </form>
-            <article class="register">
-                <p>Geen account? <a href="#">registreer nu</a></p>
-            </article>
-        </article>
-        <article class="login-image">
-            <img src="placeholder.jpg" alt="Placeholder afbeelding">
-        </article>
-        </section>
-</main>
-    <?php 
-    
-    include 'footer.php';
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Kishan & Julian">
+    <meta name="keywords" content="">
+    <title>Mbo cinema</title>
+    <link rel="stylesheet" type="text/css" href="Css/styl.css">
+</head>
+<body>
+    <?php include 'header.php'; ?>
 
-    ?>
-    </body>
+    <main class="split-section">
+        <article class="blank-section"></article>
+        <article class="form-section">
+            <section class="login-container">
+                <article class="login-form">    
+                    <h2>Login form</h2>
+                    <form>
+                        <label for="username">Gebruikersnaam</label>
+                        <input type="text" id="username" name="username" placeholder="Gebruikersnaam">
+                        
+                        <label for="password">Wachtwoord</label>
+                        <input type="password" id="password" name="password" placeholder="Wachtwoord">
+                        
+                        <article class="forgot-password">
+                            <a href="#">Wachtwoord vergeten?</a>
+                        </article>
+                        
+                        <button type="submit">Login</button>
+                    </form>
+                    <article class="register">
+                        <p><a href="#">Geen account? registreer nu</a></p>
+                    </article>
+                </article>
+            </section>
+        </article>
+        <article class="image-section"></article>
+    </main>
+
+    <?php include 'footer.php'; ?>
+</body>
 </html>
