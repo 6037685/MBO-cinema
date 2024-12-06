fb80b0b629047dd98fc80b6c97a529967cc77e10
JulKwZ <julianhoek9@gmail.com>
2024-12-06 14:50:18 +0100

    Final Touches nog later aanpassen

--- bioscoop/html/overlay.php
+++ bioscoop/html/overlay.php
@@ -4,9 +4,9 @@
     </article>
         <article id="overlay-Menu">
             <a href="Home.php">Home</a>
-            <a href="#">Films</a>
+            <a href="films.php">Films</a>
             <a href="contact.php">Contact</a>
-            <a href="#">Beveiliging</a>
+            <a href="Beveiliging.php">Beveiliging</a>
         
             <a href="login.php"><button id="loginButton">Inloggen</button></a>
         </article>
