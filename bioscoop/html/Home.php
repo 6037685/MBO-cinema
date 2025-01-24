d4871c8eac19bd26737f04c96fa272ee72fae850
JulKwZ <julianhoek9@gmail.com>
2025-01-24 13:46:41 +0100

    Terugdraaien
    Terug draai Cinema MBO

--- bioscoop/html/Home.php
+++ bioscoop/html/Home.php
@@ -1,20 +1,16 @@
 <?php
-session_start(); 
-$movies = [
-    'ratingMovies' => [
-        ['id' => 1, 'naam' => 'Joker: Folie à Deux', 'cover' => 'img/Movie-Img/Joker-Folie-à-Deux.jpg'],
-        ['id' => 2, 'naam' => 'Spider-Man: Into The Spider-Verse', 'cover' => 'img/Movie-Img/Spiderman-Into-SpiderVerse.jpg'],
-        ['id' => 3, 'naam' => 'Movie 3', 'cover' => 'img/movie3.jpg']
-    ],
-    'recentMovies' => [
-        ['id' => 4, 'naam' => 'Movie 4', 'cover' => 'img/movie4.jpg'],
-        ['id' => 5, 'naam' => 'Movie 5', 'cover' => 'img/movie5.jpg'],
-        ['id' => 6, 'naam' => 'Movie 6', 'cover' => 'img/movie6.jpg']
-    ]
-];
+session_start(); // Start the session at the beginning of the script
+require_once 'Class/User.php'; // Include the User class
+require_once 'Class/Movie.php'; // Include the Movie class
+$movie = new Movie(); // Create a new instance of the Movie class
 
-$ratingMovies = $movies['ratingMovies'];
-$recentMovies = $movies['recentMovies'];
+$recentMovies = $movie->fetchRecent();
+$ratingMovies = $movie->fetchRating();
+
+// debug code
+// echo '<pre>';
+// print_r($ratingMovies);
+// echo '</pre>';
 
 ?>
 
@@ -30,44 +26,27 @@ $recentMovies = $movies['recentMovies'];
     <link rel="stylesheet" type="text/css" href="Css/styl.css">
     <link rel="stylesheet" type="text/css" href="Css/overlay.css">
     <script defer src="js/index.js"></script>
-    <script defer src="js/slideshow.js"></script>
-
+    <script defer src="Reserveren.js"></script>
 </head>
 <body>
     <?php include_once 'header.php'; ?>
     <main>
-        <section class="slideshow-container"> 
-            <article class='slidesFade'>
-            <article class='numberText'>Dark</article>
-            <img class="slide-img" src="img/dark.jpg" alt="">
-            <article class='slide-text'>Over film</article>
-            </article>
-
-            <article class='slidesFade'>
-            <article class='numberText'>The Boys</article>
-            <img class="slide-img" src="img/the boys.jpg" alt="">
-            <article class='slide-text'>Over film</article>
-            </article>
-
-            <article class='slidesFade'>
-            <article class='numberText'>Spider-Man: Across the Spider-Verse</article>
-            <img class="slide-img" src="img/spiderman.webp" alt="">
-            <article class='slide-text'>Over film</article>
-            </article>
+        <section class="Placeholder"> 
+            <h1></h1>
         </section>
         <section class="recent-movies-container">
             <h1 id="h1-recent">
                 <a href="#">Populaire Films<i>></i></a>
             </h1>
             <section class="recent-article-container">
-                <?php for ($i = 0; $i < count($ratingMovies); $i++): ?>
+                <?php foreach ($ratingMovies as $ratingMovie): ?>
                     <article class="movie">
-                        <a href="FilmInformatie.php?id=<?php echo htmlspecialchars($ratingMovies[$i]['id'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
-                            <h2><?php echo htmlspecialchars($ratingMovies[$i]['naam'] ?? '', ENT_QUOTES, 'UTF-8'); ?></h2>
-                            <img src="<?php echo htmlspecialchars($ratingMovies[$i]['cover'] ?? '', ENT_QUOTES, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($ratingMovies[$i]['naam'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+                        <a href="FilmInformatie.php?id=<?php echo htmlspecialchars($ratingMovie['id'], ENT_QUOTES, 'UTF-8'); ?>">
+                            <h2><?php echo htmlspecialchars($ratingMovie['naam'], ENT_QUOTES, 'UTF-8'); ?></h2>
+                            <img src="<?php echo htmlspecialchars($ratingMovie['cover'], ENT_QUOTES, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($ratingMovie['naam'], ENT_QUOTES, 'UTF-8'); ?>">
                         </a>
                     </article>
-                <?php endfor; ?>
+                <?php endforeach; ?>
             </section>
         </section>
         <section class="recent-movies-container">
@@ -75,29 +54,29 @@ $recentMovies = $movies['recentMovies'];
                 <a href="#">Recent toegevoegd<i>></i></a>
             </h1>
             <section class="recent-article-container">
-                <?php for ($i = 0; $i < count($recentMovies); $i++): ?>
+                <?php foreach ($recentMovies as $recentMovie): ?>
                     <article class="movie">
-                        <a href="FilmInformatie.php?id=<?php echo htmlspecialchars($recentMovies[$i]['id'], ENT_QUOTES, 'UTF-8'); ?>">
-                            <h2><?php echo htmlspecialchars($recentMovies[$i]['naam'], ENT_QUOTES, 'UTF-8'); ?></h2>
-                            <img src="<?php echo htmlspecialchars($recentMovies[$i]['cover'], ENT_QUOTES, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($recentMovies[$i]['naam'], ENT_QUOTES, 'UTF-8'); ?>">
+                        <a href="FilmInformatie.php?id=<?php echo htmlspecialchars($recentMovie['id'], ENT_QUOTES, 'UTF-8'); ?>">
+                            <h2><?php echo htmlspecialchars($recentMovie['naam'], ENT_QUOTES, 'UTF-8'); ?></h2>
+                            <img src="<?php echo htmlspecialchars($recentMovie['cover'], ENT_QUOTES, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($recentMovie['naam'], ENT_QUOTES, 'UTF-8'); ?>">
                         </a>
                     </article>
-                <?php endfor; ?>
+                <?php endforeach; ?>
             </section>
         </section>
         <section class="info-section">
             <article id="info-article">
-                <img src="img/secured.jpeg">
+                <img src="img/Circle-placeholder.png">
                 <h1>Beveiligd</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta incidunt voluptate nostrum ullam delectus ut, eum consectetur atque accusantium eius culpa suscipit nobis voluptatum aut distinctio, fugiat magni debitis non.</p>
             </article>
             <article id="info-article">
-                <img src="img/ticket.jpeg">
+                <img src="img/Circle-placeholder.png">
                 <h1>Gemakkelijk tickets reserveren</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta incidunt voluptate nostrum ullam delectus ut, eum consectetur atque accusantium eius culpa suscipit nobis voluptatum aut distinctio, fugiat magni debitis non.</p>
             </article>
             <article id="info-article">
-                <img src="img/telefoon.png">
+                <img src="img/Circle-placeholder.png">
                 <h1>Contact</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta incidunt voluptate nostrum ullam delectus ut, eum consectetur atque accusantium eius culpa suscipit nobis voluptatum aut distinctio, fugiat magni debitis non.</p>
             </article>
